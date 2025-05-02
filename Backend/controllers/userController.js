60f77f09f8e8abcefef18d074dbb6efa8620f596
atish4752 <rohitisl2020@gmail.com>
2025-05-02 23:17:25 +0530

    update script

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -3,7 +3,7 @@ import bcrypt from "bcrypt";
 
 import jwt from "jsonwebtoken";
 import { v2 as cloudinary } from "cloudinary";
-import userModel from "../models/usermodel.js";
+import userModel from "../models/userModel.js";
 import doctorModel from "../models/doctorModel.js";
 import appointmentModel from "../models/appointmentModel.js";
 import razorpay from "razorpay";
